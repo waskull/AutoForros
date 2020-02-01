2bc9c61f76da80fd70ee0d9961295760820c4108
waskull <mrtncast@gmail.com>
2020-02-01 18:23:52 -0400

    v41

--- src/models/Inventario.ts
+++ src/models/Inventario.ts
@@ -5,7 +5,7 @@ class ModelInventario{
     private idMaterial:number = 0;
     private cantidad:number = 0;
     lista = async () => {
-        this.inventario = await pool.query("SELECT cod,cantidadStock, Color.descripcion,tipoMaterial.descrip FROM Inventario,Color,tipoMaterial,Material Where Inventario.id_material=Material.idMaterial and Material.idTipo=tipoMaterial.idTipo and Color.idColor=Material.color");
+        this.inventario = await pool.query("SELECT cod,cantidadStock, Colores.descripcion,tipoMaterial.descrip FROM Inventario,Colores,tipoMaterial,Materiales Where Inventario.id_material=Materiales.idMaterial and Materiales.idTipo=tipoMaterial.idTipo and Colores.idColor=Materiales.color");
         return this.inventario;
     };
     public setIdMaterial(idMaterial:number){
