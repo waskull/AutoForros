a3a6963adbff97f465cd1518292142b1e982618d
Martin C <58961096+waskull@users.noreply.github.com>
2020-02-28 05:28:58 -0400

    agregado traducción en español

--- src/lib/handlebars.ts
+++ src/lib/handlebars.ts
@@ -1,10 +1,28 @@
-const {format} = require('timeago.js');
+const {format,register} = require('timeago.js');
+
 const helpers = {
-  timestamp(timestamp:any){
-    return format(timestamp);
-  },
   timeago(timestamp:any){
-    return format(timestamp);
+  	const localeFunc = (number: number, index: number, totalSec: number): [string, string] => {
+  return [
+    ['justo ahora', 'en un rato'],
+    ['hace %s segundos', 'en %s segundos'],
+    ['hace 1 minuto', 'en 1 minuto'],
+    ['hace %s minutos', 'en %s minutos'],
+    ['hace 1 hora', 'en 1 hora'],
+    ['hace %s horas', 'en %s horas'],
+    ['hace 1 día', 'en 1 día'],
+    ['hace %s días', 'en %s días'],
+    ['hace 1 semana', 'en 1 semana'],
+    ['hace %s semanas', 'en %s semanas'],
+    ['hace 1 mes', 'en 1 mes'],
+    ['hace %s meses', 'en %s meses'],
+    ['hace 1 año', 'en 1 año'],
+    ['hace %s años', 'en %s años'],
+  ][index] as [string, string];
+};
+// register your locale with timeago
+register('es_ES', localeFunc);
+    return format(timestamp, 'es_ES');
   }
 }
 export default helpers;
